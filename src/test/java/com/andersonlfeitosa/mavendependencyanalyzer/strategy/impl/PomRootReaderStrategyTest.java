e773418928a4aff967464a3fdec74e7b68eed452
Anderson Lobo Feitosa <andersonlf@gmail.com>
2014-03-10 17:48:58 -0300

    new unit tests

--- src/test/java/com/andersonlfeitosa/mavendependencyanalyzer/strategy/impl/PomRootReaderStrategyTest.java
+++ src/test/java/com/andersonlfeitosa/mavendependencyanalyzer/strategy/impl/PomRootReaderStrategyTest.java
@@ -99,6 +99,54 @@ public class PomRootReaderStrategyTest {
 		assertEquals("1.0.0.0-SNAPSHOT", project2.getVersion());
 		
 	}
+	
+	@Test
+	public void testReadApp3() {
+		File file = createFile("poms/app3/pom.xml");
+		Map<String, Project> poms = reader.read(file); 
+		assertEquals(4, poms.values().size());
+		
+		Project project = poms.get("com.app3:app3:1.0.0.0-SNAPSHOT");
+		
+		assertEquals(null, project.getAggregatorProject());
+		assertEquals(project.getArtifactId(), "app3");
+		assertEquals(null, project.getDependencies());
+		assertEquals(null, project.getDependencyManagement());
+		assertEquals(project.getGroupId(), "com.app3");
+		assertEquals("4.0.0", project.getModelVersion());
+		assertEquals(1, project.getModules().size());
+		assertEquals("app3", project.getName());
+		assertEquals("pom", project.getPackaging());
+		assertEquals("com.pom", project.getParent().getGroupId());
+		assertEquals("super-pom", project.getParent().getArtifactId());
+		assertEquals("1.0.0.0", project.getParent().getVersion());
+		assertEquals(null, project.getParentProject());
+		assertEquals("com.app3", project.getProperties().get("project.groupId").getValue());
+		assertEquals("app3", project.getProperties().get("project.artifactId").getValue());
+		assertEquals("1.0.0.0-SNAPSHOT", project.getProperties().get("project.version").getValue());
+		assertEquals("1.0.0.0-SNAPSHOT", project.getVersion());
+		
+		Project project2 = poms.get("com.app3:mod1:1.0.0.0-SNAPSHOT");
+		assertEquals(project, project2.getAggregatorProject());
+		assertEquals(project2.getArtifactId(), "mod1");
+		assertEquals(null, project2.getDependencies());
+		assertEquals(null, project2.getDependencyManagement());
+		assertEquals(project2.getGroupId(), "com.app2");
+		assertEquals("4.0.0", project2.getModelVersion());
+		assertEquals(1, project2.getModules().size());
+		assertEquals("mod1", project2.getName());
+		assertEquals("pom", project2.getPackaging());
+		assertEquals("com.app2", project2.getParent().getGroupId());
+		assertEquals("app2", project2.getParent().getArtifactId());
+		assertEquals("1.0.0.0-SNAPSHOT", project2.getParent().getVersion());
+		assertEquals(project, project2.getParentProject());
+		assertEquals("com.app2", project2.getProperties().get("project.groupId").getValue());
+		assertEquals("mod1", project2.getProperties().get("project.artifactId").getValue());
+		assertEquals("1.0.0.0-SNAPSHOT", project2.getProperties().get("project.version").getValue());
+		assertEquals("1.0.0.0-SNAPSHOT", project2.getVersion());
+		
+	}
+	
 
 	private File createFile(String path) {
 		File file = null;
